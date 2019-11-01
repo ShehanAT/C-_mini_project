21555068dcda49accdb540c542f7208139b6be6e
Shehan Atuk <shehanatuk@gmail.com>
2019-11-01 17:02:29 -0400

    added first project file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testing1
+//
+//  Created by Shehan Atuk on 2019-10-30.
+//  Copyright © 2019 Shehan Atuk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Welcome to C++\n";
+    return 0;
+}
